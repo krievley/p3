36cadcadadc868bebfe8f2f79d51eae20213d6ae
Kristin Rievley <krievley@gmail.com>
2014-10-18 23:13:51 -0400

    Added homepage view

---
+++ app/views/index.blade.php
@@ -0,0 +1,56 @@
+﻿@extends('master')
+
+@section('picture')
+    <img id="toolbox" src="{{ asset('images/itToolbox.png')}}" alt="IT Toolbox" />
+@stop
+
+@section('h1')
+    <h1>Developer Tools</h1>
+@stop
+
+@section('content')
+    <div id="main">
+        <div class="container">
+            <div class="col2">
+                <h2>Lorem Ipsum Generator</h2>
+                <p>
+                    This tool generates lorem ipsum text in the number of paragraphs specified by the user.
+                    Lorem ipsum is filler text used to represent content on a page. It is used to show where
+                    content will go without using content that might distract a client.
+                </p>
+            </div>
+            <div class="col1">
+                <img id="robot" src="{{ asset('images/robot.jpg') }}" alt="Robot" />
+            </div>
+        </div>
+    </div>
+    <div id="secondary">
+        <div class="container">
+            <div class="col1">
+                <img id="cloud" src="{{ asset('images/cloud.png') }}" alt="Cloud" />
+            </div>
+            <div class="col2">
+                <h2>Random User Generator</h2>
+                <p>
+                    This tool generates random user information. This is helpful for developers
+                    who need dummy user information to test their applications.
+                </p>
+            </div>
+        </div>
+    </div>
+    <div id="final">
+        <div class="container">
+            <div class="col2">
+                <h2>XKCD Password Generator</h2>
+                <p>
+                    This tool generates four random words with the option to add a symbol and number.
+                    This password idea is based on the assumption that four words are easy to remember 
+                    but hard to guess.
+                </p>
+            </div>
+            <div class="col1">
+                <img id="wrench" src="{{ asset('images/wrench.jpg') }}" alt="Wrench" />
+            </div>
+        </div>
+    </div>
+@stop
---
+++ app/views/master.blade.php
@@ -0,0 +1,26 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>@yield('title', 'p3: CSCI E-25')</title>
+    <link rel='stylesheet' href='{{ asset('css/normalize.css') }}'/>
+    <link rel='stylesheet' href='{{ asset('css/default.css') }}'/>
+</head>
+<body>
+    <div id="header">
+        <div class="container">
+            <div class="col3">
+                @yield('picture')
+            </div>
+            <div class="col3">
+                @yield('h1')
+                <h4>Home | Lorem Ipsum | Random User | XKCD Password</h4>
+            </div>
+        </div>
+        <div id="bar"></div>
+    </div>
+    @yield('content')
+    <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js' type='text/javascript'></script>
+</body>
+</html>
